84deabf3bad12c344d9c49096f35288199b8b2d1
Arush <55015285+aruahaha@users.noreply.github.com>
2024-09-28 19:49:46 +0530

    socket cors

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const server = app.listen(process.env.PORT, () => {
 
 const io = socket(server, {
   cors: {
-    origin: "",
+    origin: "https://talk-mate-five.vercel.app",
     credentials: true,
   },
 });
